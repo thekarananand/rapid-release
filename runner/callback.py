f2ae6164352fe54c99fb2cb241f43b4f74232f02
Karan Anand <thekarananand@gmail.com>
2025-04-26 20:30:29 +0530

    feat: add Elasticsearch && Kibana

--- runner/callback.py
+++ runner/callback.py
@@ -1,6 +1,32 @@
 import json
 import subprocess
 import threading
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(
+    "http://elasticsearch:9200",
+    headers={
+        "Content-Type": "application/json",
+        "Accept":       "application/json"
+    }
+)
+
+def logHandeler(stream, label):
+
+    for line in stream:
+        log_line = line.strip()
+        print(f"[{label}] {log_line}")
+
+        es.index(
+            index="logs",
+            document={
+                "service": "runner",
+                "stream": label,
+                "message": log_line,
+                "timestamp": datetime.utcnow()
+            }
+        )
 
 def callback(channel, method, properties, body):
     try:
@@ -19,12 +45,8 @@ def callback(channel, method, properties, body):
             bufsize=1
         )
 
-        def stream_output(stream, label):
-            for line in stream:
-                print(f"[{label}] {line.strip()}")
-
-        stdout_thread = threading.Thread(target=stream_output, args=(process.stdout, "STDOUT"))
-        stderr_thread = threading.Thread(target=stream_output, args=(process.stderr, "STDERR"))
+        stdout_thread = threading.Thread(target=logHandeler, args=(process.stdout, "STDOUT"))
+        stderr_thread = threading.Thread(target=logHandeler, args=(process.stderr, "STDERR"))
 
         stdout_thread.start()
         stderr_thread.start()
@@ -43,7 +65,6 @@ def callback(channel, method, properties, body):
         print(f"Error during processing: {e}")
 
     finally:
-        # Stop consuming ONLY after work is done
         try:
             channel.stop_consuming()
         except Exception as e:
